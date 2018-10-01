7b2ebe07d3aa04cc2d7d765bcd8adfd0cfd8b5ab
kakeru.seto1234@gmail.com <31640826k>
2018-10-01 10:49:42 +0900

    2
    2

--- output.php
+++ output.php
@@ -1,7 +1,19 @@
 <!-- <?php
+require_once('./output_functions.php');
+session_start();
+redirectIfNotLogin();
+$id = $_SESSION['user']['id'];
+$username = $_SESSION['user']['username'];
+$nichiji  = date('Y-m-d H:i:s');
+$pdo = connectDB();
 if($_SERVER["REQUEST_METHOD"]==="POST"){
 
 }
+$sql="SELECT * FROM stations"
+$statement=$pdo->prepare($sql);
+$statement->execute();
+$stations=$statement->fetchAll();
+
 
 ?> -->
 <!DOCTYPE html>
@@ -18,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
    opacity: 0.8;
    border-radius: 4px;
  }
- //>
+
  </style>
  <title>ペルセポネ</title>
 </head>
--- output_function.php
+++ output_function.php
@@ -2,7 +2,29 @@
 function connectDB()
 {
   require_once('./output_function.php');
-  $dsn="mysql:dbname=$db_name;host=$host".'charset=utf8'
-  try{}catch(PDOException $e){return $pdo;}
+  $dsn="mysql:dbname=$db_name;host=$host".';charset=utf8'
+  try{
+    $pdo = new PDO($dsn, $db_user, $db_passwd);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+  }catch(PDOException $e){
+    die ('DB Connection Failed')
+  }
+    return $pdo;
+}
+
+function h($string){
+  return htmlspecialchars($string,ENT_QUOTES,'UTF-8');
+}
+
+function redirectIfNotLogin()
+{
+  if (!isset($_SESSION['user'])){
+    header('Location: login.php');
+    return;
+  }
+}
+
+function loginUser(){
+  return $_SESSION['user'];
 }
 ?>
